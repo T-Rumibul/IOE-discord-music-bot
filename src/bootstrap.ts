33acb5a4dd61d563d0d194813c9466b241d1d0b5
T-Rumibul <ryner.no@gmail.com>
2026-08-09 12:59:49 +0200

    ♻️ refactor(main): move initialization logic to bootstrap.ts and update entry points in package.json

---
+++ src/bootstrap.ts
@@ -0,0 +1,25 @@
+import { BinaryState, checkBinary, downloadBinary } from "./utils/ytdlpBinaries.js";
+import fs from "fs";
+import path from "path";
+import { getConfig } from "./config.js";
+const config = getConfig();
+const ytdlpBinaryState = await checkBinary()
+if (ytdlpBinaryState === BinaryState.NEED_UPDATE || ytdlpBinaryState === BinaryState.NOT_FOUND) {
+    await downloadBinary()
+}
+
+const logDirIsExists = fs.existsSync(path.join(process.cwd(), config.LOG_FOLDER))
+const downloadsDirIsExists = fs.existsSync(path.join(process.cwd(), config.DOWNLOADS_FOLDER))
+if (!logDirIsExists) {
+    fs.mkdirSync(path.join(process.cwd(), config.LOG_FOLDER))
+}
+if (!downloadsDirIsExists) {
+    fs.mkdirSync(path.join(process.cwd(), config.DOWNLOADS_FOLDER))
+}
+
+try {
+    const { default: main } = await import("./main.js");
+    await main();
+} catch (e) {
+    console.error(e, "Failed to start main");
+}
--- src/main.ts
+++ src/main.ts
@@ -1,32 +1,15 @@
-import fs from "fs";
-import path from "path/win32";
 import { IOEClient } from "./client/IOEClient.js";
 import { logger } from './utils/index.js';
 import { startServer } from "./server.js";
 import { getConfig } from "./config.js";
-import { downloadBinary, checkBinary, BinaryState } from "./utils/ytdlpBinaries.js";
-
 
 const config = getConfig();
 async function main() {
-    const ytdlpBinaryState = await checkBinary()
-    if (ytdlpBinaryState === BinaryState.NEED_UPDATE || ytdlpBinaryState === BinaryState.NOT_FOUND) {
-        await downloadBinary()
-    }
-
     if (!config.DOWNLOADS_COMMAND_DISABLED) {
         await startServer().catch(e => logger.error(e, 'Failed to start server'));
-    }
-    const logDirIsExists = fs.existsSync(path.join(process.cwd(), config.LOG_FOLDER))
-    const downloadsDirIsExists = fs.existsSync(path.join(process.cwd(), config.DOWNLOADS_FOLDER))
-    if (!logDirIsExists) {
-        fs.mkdirSync(path.join(process.cwd(), config.LOG_FOLDER))
-    }
-    if (!downloadsDirIsExists) {
-        fs.mkdirSync(path.join(process.cwd(), config.DOWNLOADS_FOLDER))
     }
     const client = new IOEClient(config.CLIENT_ID, config.TOKEN);
     client.login();
 }
 
-main()
+export default main
