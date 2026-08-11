714f148bd31c4be5640312cabdbe79cf5ca5641c
T-Rumibul <ryner.no@gmail.com>
2026-08-11 15:36:39 +0200

    ♻️ refactor(IOEClientDatabase): update database logging to use logger

--- src/client/IOEClientDatabase.ts
+++ src/client/IOEClientDatabase.ts
@@ -5,7 +5,7 @@ import type { IOEClient } from './IOEClient.js';
 const dbPath = path.join(import.meta.dirname, '..', '..', '/database.db');
 
 // open the database
-const db = new Database(dbPath, { verbose: (msg) => console.log(msg) });
+const db = new Database(dbPath, { verbose: (msg) => logger.debug(msg, 'Database') });
 
 await db.exec('CREATE TABLE IF NOT EXISTS guilds (guild_id TEXT, music_channel TEXT, PRIMARY KEY(guild_id))');
 export class IOEClientDatabase {
